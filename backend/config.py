dd829b16abd8f58254f363a6b684048a2068ff42
muudzo <tatendawalter62@gmail.com>
2026-01-11 23:04:17 +0800

    backend: configure environment loading and settings object

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    app_nam: str = "Nowhere"
+    debug: bool = False
+    
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
