6072d65221481603f779e79370c8648f7d3c3cb8
muudzo <tatendawalter62@gmail.com>
2026-03-20 11:44:27 +0200

    feat(jokes): add cow joke #3

--- app/hooks/useCowJokes.ts
+++ app/hooks/useCowJokes.ts
@@ -5,3 +5,4 @@ export const useCowJokes = () => {
   const [jokes] = useState<string[]>([
     "What do you call a cow with no legs? Ground beef.",
     "What do you call a cow with a twitch? Beef jerky.",
+    "What do you get from a pampered cow? Spoiled milk.",
